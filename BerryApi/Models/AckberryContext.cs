0257d74b58a1d50c870b0e747981ffa114c5ec32
Gilbert Mule <gmule@directcore.com>
2020-06-30 22:22:43 +0300

    First Commit

---
+++ BerryApi/Models/AckberryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BerryApi.Models
+{
+
+    public class AckberryContext : IdentityDbContext
+    {
+        public AckberryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<ToDoItem> ToDoItem { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ BerryApi/Models/ApplicationUserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BerryApi.Models
+{
+    public class ApplicationUserModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
